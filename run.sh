56885865103f628af7f3f346b80b6821170565f9
Florent Dufour <florent.dufour@lrz.de>
2023-12-01 14:35:19 +0100

    Doucement.

--- run.sh
+++ run.sh
@@ -3,8 +3,12 @@
 curl -fsSL https://get.docker.com | bash
 docker compose up -d
 
+# Doucement
+sleep 10
 NOTEBOOK=`docker compose exec notebook jupyter notebook list`
+sleep 2
 TOKEN=$(echo $NOTEBOOK | grep -oP '(?<=token=)[a-zA-Z0-9]+')
+sleep 2
 IP=`curl 169.254.169.254/latest/meta-data/public-ipv4`
 
 echo -e "\n"
