dade0ead936b0273e0737b3b3e60c5ad667dc7e9
Florent Dufour <florent.dufour@lrz.de>
2023-12-01 10:45:22 +0100

    Import

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker compose up -d
+
+NOTEBOOK=`docker compose exec notebook jupyter notebook list`
+TOKEN=$(echo $NOTEBOOK | grep -oP '(?<=token=)[a-zA-Z0-9]+')
+IP=`curl 169.254.169.254/latest/meta-data/public-ipv4`
+
+echo -e "\n"
+echo "http://"$IP":8888/?token="$TOKEN
+echo -e "\n"
